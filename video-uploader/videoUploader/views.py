e436bb2789219cf956256220802e2ed0dfb304a6
Raushan Kumar <kumarraushanrk5@gmail.com>
2020-08-05 15:02:36 +0530

    modified all the files

--- video-uploader/videoUploader/views.py
+++ video-uploader/videoUploader/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from sprout.client import SproutClient
+# from urlparse import urljoin
 
 # Create your views here.
 def home(request):
@@ -8,8 +12,24 @@ def home(request):
 def contact(request):
     return render(request,'contact.html')
 
-def index(reqest):
-    return render(reqest,'index.html')
+def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        sprout = SproutClient("948ea6bdb9ac2b12a1b045108b13c648")
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        # Upload video
+        response =  sprout.video.upload('media/'+ filename, title="Rohitt!")
+        for key, value in response.items(): 
+            if (str(key) == "id"):
+                print("id") 
+                print(value)
+            if (str(key) == "security_token"): 
+                print("token")
+                print(value)  
+            if (str(key) == 'embed_code'): 
+                print(value) 
+    return render(request,'index.html')
 
 def view(request):
     return render(request,'view.html')
