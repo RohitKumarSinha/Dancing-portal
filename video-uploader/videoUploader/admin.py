3f327fe435ee72b4783a7425c0a1ca9d5a14fedf
unknown <sinharohit3333@gmail.com>
2020-08-06 12:02:47 +0530

    add-functnality

--- video-uploader/videoUploader/admin.py
+++ video-uploader/videoUploader/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import upload_video
 
 # Register your models here.
+# class VideoAdmin(admin.ModelAdmin):
+#   list_display = ('id')
+
+admin.site.register(upload_video)
